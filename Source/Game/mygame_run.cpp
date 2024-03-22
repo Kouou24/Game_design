5da75ba0111b89ed9c8049ae775ab10f0d3e7c2d
Wei <t111590023@ntut.org.tw>
2024-03-22 11:06:39 +0800

    flag reset

--- Source/Game/mygame_run.cpp
+++ Source/Game/mygame_run.cpp
@@ -206,7 +206,6 @@ void CGameStateRun::OnShow()
 {
 	phase_control();
 	show_image_by_phase();
-	
 	show_text_by_phase();
 }
 
@@ -255,23 +254,19 @@ void CGameStateRun::show_text_by_phase() {
 		CTextDraw::Print(pDC, 50, 20, "關卡 : 2 / 20");
 		CTextDraw::Print(pDC, 370, 20, "將箱子推到指定地點");
 	} else if (phase == 3 && sub_phase == 1) {
-		CTextDraw::Print(pDC, 205, 128, "");
-		CTextDraw::Print(pDC, 68, 162, "");
-		CTextDraw::Print(pDC, 68, 196, "");
-		CTextDraw::Print(pDC, 373, 537, "");
+		CTextDraw::Print(pDC, 50, 20, "關卡 : 3 / 20");
+		CTextDraw::Print(pDC, 370, 20, "將箱子推到指定地點");
+
 	} else if (phase == 4 && sub_phase == 1) {
-		CTextDraw::Print(pDC, 173, 128, "");
-		CTextDraw::Print(pDC, 89, 162, "");
-		CTextDraw::Print(pDC, 110, 196, "");
-		CTextDraw::Print(pDC, 373, 537, "");
+		CTextDraw::Print(pDC, 50, 20, "關卡 : 4 / 20");
+		CTextDraw::Print(pDC, 370, 20, "將箱子推到指定地點");
+
 	} else if (phase == 5 && sub_phase == 1) {
-		CTextDraw::Print(pDC, 173, 128, "幫你準備了三扇門");
-		CTextDraw::Print(pDC, 89, 162, "設計程式讓長頸鹿摸到門之後，門會打開");
-		CTextDraw::Print(pDC, 373, 537, "按下 Enter 鍵來驗證");
+		CTextDraw::Print(pDC, 50, 20, "關卡 : 5 / 20");
+		CTextDraw::Print(pDC, 370, 20, "將箱子推到指定地點");
 	} else if (phase == 6 && sub_phase == 1) {
-		CTextDraw::Print(pDC, 173, 128, "幫你準備了一顆會倒數的球");
-		CTextDraw::Print(pDC, 89, 162, "設計程式讓球倒數，然後顯示 OK 後停止動畫");
-		CTextDraw::Print(pDC, 373, 537, "按下 Enter 鍵來驗證");
+		CTextDraw::Print(pDC, 50, 20, "關卡 : 6 / 20");
+		CTextDraw::Print(pDC, 370, 20, "將箱子推到指定地點");
 	} 
 
 	CDDraw::ReleaseBackCDC();
@@ -318,7 +313,7 @@ void CGameStateRun::reset_phase() {
 
 			}
 			if (map[i][j] == 5) {
-
+				fin.SetFrameIndexOfBitmap(0);
 				fin.SetTopLeft(150 + MAP_SIZE * i, 150 + MAP_SIZE * i);
 
 			}
@@ -335,51 +330,59 @@ void CGameStateRun::phase_control() {
 		}
 		else if (sub_phase == 2) {
 			sub_phase = 1;
+			win_flag = 0;
 			phase += 1;
 		}
 	}
+	
 	else if (phase == 2) {
+
 		if (sub_phase == 1) {
-			sub_phase += validate_phase_2();
+			sub_phase += win_flag;
 		}
 		else if (sub_phase == 2) {
 			sub_phase = 1;
+			win_flag = 0;
 			phase += 1;
 		}
 	}
 	else if (phase == 3) {
 		if (sub_phase == 1) {
-			sub_phase += validate_phase_3();
+			sub_phase += win_flag;
 		}
 		else if (sub_phase == 2) {
 			sub_phase = 1;
+			win_flag = 0;
 			phase += 1;
 		}
 	}
 	else if (phase == 4) {
 		if (sub_phase == 1) {
-			sub_phase += validate_phase_4();
+			sub_phase += win_flag;
 		}
 		else if (sub_phase == 2) {
 			sub_phase = 1;
+			win_flag = 0;
 			phase += 1;
 		}
 	}
 	else if (phase == 5) {
 		if (sub_phase == 1) {
-			sub_phase += validate_phase_5();
+			sub_phase += win_flag;
 		}
 		else if (sub_phase == 2) {
 			sub_phase = 1;
+			win_flag = 0;
 			phase += 1;
 		}
 	}
 	else if (phase == 6) {
 		if (sub_phase == 1) {
-			sub_phase += validate_phase_6();
+			sub_phase += win_flag;
 		}
 		else if (sub_phase == 2) {
 			sub_phase = 1;
+			win_flag = 0;
 			phase += 1;
 			GotoGameState(GAME_STATE_OVER);
 		}
